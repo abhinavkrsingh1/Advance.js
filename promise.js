07989902fc9ac4dc6f7f80f2f833a74fc12f910b
Abhinav kumar <abhinavbaddu01@gmail.com>
2026-04-03 13:55:17 +0530

    learn/something

--- promise.js
+++ promise.js
@@ -1,22 +1,22 @@
-// console.log("Start");
-// const myPromise = new Promise((resolve,reject)=>{
-//     success = true;
-//     setTimeout(()=>{
-//       if(success){
-//         resolve("Promise Successfull");
-//       }
-//       else{
-//         reject(new Error("Promise Rejected"))
-//       }
-//     },2000);
-// })
-// console.log("Ended");
-// myPromise.then((result)=>{
-//     console.log("Success",success);
-// })
-// myPromise.catch((error)=>{
-//     console.log("Success",error);
-// })
+console.log("Start");
+const myPromise = new Promise((resolve,reject)=>{
+    success = true;
+    setTimeout(()=>{
+      if(success){
+        resolve("Promise Successfull");
+      }
+      else{
+        reject(new Error("Promise Rejected"))
+      }
+    },2000);
+})
+console.log("Ended");
+myPromise.then((result)=>{
+    console.log("Success",success);
+})
+myPromise.catch((error)=>{
+    console.log("Success",error);
+})
 console.log("start");
 function promise(){
   return new Promise((resolve,reject)=>{
@@ -40,3 +40,23 @@ promise().then(()=>{
   console.log("reject");
 })
 console.log("end");
+function getnum(n,delay) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (n % 2 === 0) {
+        resolve(n * n);
+      } else {
+        reject("odd");
+      }
+    }, delay);
+  });
+  return promise
+}
+
+async function getData(){
+  const  res = await getnum(4,1000);
+  console.log(res);
+  const res2 = await getnum(res);
+  console.log(res2);
+}
+getData()
--- reverse.js
+++ reverse.js
@@ -1,7 +1,24 @@
-function isreverse(str){
-  let result = str.split("").reverse().join("")
-  return result
-}
+// function isreverse(str){
+//   let result = str.split("").reverse().join("")
+//   return result
+// }
 
-let str = isreverse("hello")
-console.log(str);
+// let str = isreverse("hello")
+// console.log(str);
+
+
+const arr = [23,41,25,49,47]
+// const res  = Math.max(...arr)
+// console.log(res);
+
+
+let max = 0;
+for(let i=0;i<=arr.length;i++){
+
+     if(arr[i] > max){
+      max = arr[i];
+     }
+   
+    
+}
+console.log(max);
